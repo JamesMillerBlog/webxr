a05a3251e234c424c81c9e5644a93b01149443cf
James Miller <hi@jamesmiller.blog>
2023-11-29 18:56:06 +0000

    feat: cd buckets

--- src/shared/infrastructure/resources/SecretsManager.ts
+++ src/shared/infrastructure/resources/SecretsManager.ts
@@ -6,17 +6,8 @@ import { ClientSecrets, ServerSecrets } from '../types'
 export class SecretsManager {
   name: string;
   constructor(name: string, secrets: ClientSecrets | ServerSecrets, parent: Shared) {
-    // needs updating
     this.name = name;
     const secret = new aws.secretsmanager.Secret(`${this.name}`, {name: this.name}, {parent});
-    
-    /* 
-      2. GITHUB CI/CD (add way to create environments through Github UI)
-
-
-
-    npm run scripts:generate:client-and-server-envs webxr-shared-dev development
-    */
 
     new aws.secretsmanager.SecretVersion(`${name}_secrets_version`, {
         secretId: secret.id,
